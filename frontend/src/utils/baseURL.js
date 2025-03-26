6a42437c6817fdd4a05482ce738318cee69265ae
dengluxurye <dluxurymou@gmail.comgit config --global user.email dluxurymou@gmail.com>
2025-03-26 16:04:01 +0200

    fi

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,18 +1,18 @@
-// const getBaseUrl = () => {
-//      return "http://localhost:5000";
-// }
-
-// export default getBaseUrl;
-
-
-
-
 const getBaseUrl = () => {
-     const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://backend-app-ecc8.onrender.com";
-     return backendUrl;
- }
+     return "http://localhost:5000";
+}
+
+export default getBaseUrl;
+
+
+
+
+// const getBaseUrl = () => {
+//      const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://backend-3-byl7.onrender.com";
+//      return backendUrl;
+//  }
  
- export default getBaseUrl;
+//  export default getBaseUrl;
  
 
 
